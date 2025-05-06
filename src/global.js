baf78e6c6094b1ad8483d1b69e241e850614d2e8
Anthony <anthonyberri030@gmail.com>
2025-05-06 20:27:06 -0300

    global header OK

--- src/global.js
+++ src/global.js
@@ -1,3 +1,38 @@
+//HTML HEADER
+function globalHeader(){
+    return`
+        <button class="menu-btn">☰ </button> 
+    
+        <img src="../img/logo.png" alt="Logo" class="logo">
+    
+        <div class="login-btn">  <!-- Botão de login estilizado -->
+            <a href="../login/login.html" style="color: inherit; font-size: 35px;">
+                <i class="bi bi-person-circle"></i>
+            </a>
+        </div>
+    
+         <!-- Sidebar -->
+        <div class="sidebar">
+            <ul>               
+                <li><a href="../home/index.html">Home</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Carros</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </div> 
+    `;
+}
+
+//GET HEADER FROM HTML CODE
+
+const header = document.getElementById("header");
+
+//SET HEADER ON HTML CODE
+
+header.innerHTML = globalHeader();
+
+//SET SIDEBAR
+
 const menuBtn = document.querySelector('.menu-btn');
 const sidebar = document.querySelector('.sidebar');
 
