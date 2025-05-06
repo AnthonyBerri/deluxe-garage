c14f6444e4f39b6a67be5b3545c6edfffb493255
Anthony <anthonyberri030@gmail.com>
2025-05-06 19:14:11 -0300

    global.js coloque scripts globais por exemplo a sidebar

--- src/home/script.js
+++ src/home/script.js
@@ -41,11 +41,3 @@ prevButton.onclick = function () {
         }
     }
 };
-
- // JavaScript to toggle the sidebar
- const menuBtn = document.querySelector('.menu-btn');
- const sidebar = document.querySelector('.sidebar');
-
- menuBtn.addEventListener('click', () => {
-     sidebar.classList.toggle('show');
- });
