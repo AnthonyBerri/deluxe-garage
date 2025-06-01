415c901bb37654895a4a46eea1d68e973fb622c8
arthurfaust <arthur.f.locatelli@gmail.com>
2025-06-01 20:43:29 -0300

    rascunho do footer

---
+++ components/footer/footer.js
@@ -0,0 +1,29 @@
+//HTML FOOTER
+class FooterComponent extends HTMLElement {
+    constructor() {
+        super();
+    }
+    connectedCallback() {
+        this.innerHTML = `
+            <footer style="background-color: #333; color: white; padding: 20px; text-align: center;">
+                <div>
+                    <p>&copy; 2025 Deluxe Garage. Todos os direitos reservados.</p>
+                    <p>
+                        <a href="https://www.instagram.com" target="_blank" style="color: #fff; margin: 0 10px;">
+                            <i class="bi bi-instagram"></i>
+                        </a>
+                        <a href="https://www.facebook.com" target="_blank" style="color: #fff; margin: 0 10px;">
+                            <i class="bi bi-facebook"></i>
+                        </a>
+                        <a href="https://www.twitter.com" target="_blank" style="color: #fff; margin: 0 10px;">
+                            <i class="bi bi-twitter-x"></i>
+                        </a>
+                    </p>
+                </div>
+            </footer>
+        `;
+    }
+}
+
+// Registrar o componente
+customElements.define('footer-component', FooterComponent);
