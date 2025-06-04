bacc35eeb5fea5f1fb0fd6decb3640256bbce9a1
Anthony <anthonyberri030@gmail.com>
2025-06-03 22:04:57 -0300

    Commit Legal (Aprovado pelo professor, confia)

--- pages/brands/brands.js
+++ pages/brands/brands.js
@@ -1,24 +1,38 @@
 const params = new URLSearchParams(window.location.search);
 const brandName = params.get('name');
 
-fetch('brand.json') 
-    .then(response => response.json())
-    .then(brands => {
-        const brand = brands.find(m => m.name.toLowerCase() === brandName.toLowerCase());
+if (!brandName) {
+    const div = document.getElementById('info');
+    if (div) {
+        div.innerHTML = "<p>Marca não especificada na URL.</p>";
+    }
+} else {
+    fetch('/pages/brands/brand.json') 
+        .then(response => response.json())
+        .then(brands => {
+            const brand = brands.find(m => m.name.toLowerCase() === brandName.toLowerCase());
 
-        const div = document.getElementById('info');
+            const main = document.getElementById('info');
 
-        if(brand) {
-            div.innerHTML = `
-                <!-- <header-component></header-component> -->
-                <h2>${brand.name}</h2>
-                <img src="${brand.logo}" alt="${brand.name}">
-            `;
-        } else {
-            div.innerHTML = `<p>Marca não encontrada</p>`; 
-        }
-    })
-    .catch(error => {
-    document.getElementById('info').innerText = 'Erro ao carregar os dados.';
-    console.error('Erro:', error);
-  });
+            if (main) {
+                if (brand) {
+                    main.innerHTML = `
+                        <div class="card">
+                            <img src="${brand.logo}" alt="${brand.name}" class="brand-logo">
+                            <carrossel-component></carrossel-component>
+                            <cards-container></cards-container>
+                        </div>
+                    `;
+                } else {
+                    div.innerHTML = `<p>Marca não encontrada</p>`; 
+                }
+            }
+        })
+        .catch(error => {
+            const div = document.getElementById('info');
+            if (div) {
+                div.innerText = "Erro ao carregar os dados.";
+            }
+            console.error('Erro:', error);
+        });
+}
