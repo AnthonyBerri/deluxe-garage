3a75529e62e038f1f166057983a302e33f6bd6ac
arthurfaust <arthur.f.locatelli>
2025-06-03 21:21:00 -0300

    Atualização do footer

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -2,15 +2,14 @@
 class FooterComponent extends HTMLElement {
     constructor() {
         super();
-    }
-    connectedCallback() {
-        this.innerHTML = `
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.innerHTML = `
             <footer style="background-color: #333; color: white; padding: 20px; text-align: center;">
                 <div>
                     <p>&copy; 2025 Deluxe Garage. Todos os direitos reservados.</p>
                     <p>
                         <a href="https://www.instagram.com" target="_blank" style="color: #fff; margin: 0 10px;">
-                            <i class="bi bi-instagram"></i>
+                            <i class="fa-brands fa-instagram"></i>
                         </a>
                         <a href="https://www.facebook.com" target="_blank" style="color: #fff; margin: 0 10px;">
                             <i class="bi bi-facebook"></i>
