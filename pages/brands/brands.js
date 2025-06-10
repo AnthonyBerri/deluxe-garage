1140a3874be1f48668f38027b561dd8eaaf54d9f
GolfinhoVoador <marcosantoniomartinsbr1111@gmail.com>
2025-06-10 20:38:38 -0300

    Reverter Merge Anterior

--- pages/brands/brands.js
+++ pages/brands/brands.js
@@ -1,43 +1,38 @@
-    const params = new URLSearchParams(window.location.search);
-    const brandId = params.get('id');
+const params = new URLSearchParams(window.location.search);
+const brandId = params.get('id');
 
-    if (!brandId) {
-        const div = document.getElementById('info');
-        if (div) {
-            div.innerHTML = "<p>Marca não especificada na URL.</p>";
-        }
-    } else {
-        fetch('/pages/brands/brand.json') 
-            .then(response => response.json())
-            .then(brands => {
-                const brand = brands.find(m => m.id === brandId);
-
-                const main = document.getElementById('info');
-
-                if (main) {
-                    if (brand) {
-                        main.innerHTML = `
-                            <div class="card">
-                                <img src="${brand.logo}" alt="${brand.name}" class="brand-logo">
-                                <carrossel-component></carrossel-component>
-                                <cards-brands></cards-brands>  
-                            </div>
-                        `;
-
-                        const cardsContainer = document.querySelector('cards-brands');
-                        if (cardsContainer && brand.cars) {
-                            cardsContainer.setAttribute('data-cars', JSON.stringify(brand.cars));
-                        }
-                    } else {
-                        div.innerHTML = `<p>Marca não encontrada</p>`; 
-                    }
-                }   
-            })
-            .catch(error => {
-                const div = document.getElementById('info');
-                if (div) {
-                    div.innerText = "Erro ao carregar os dados.";
-                }
-                console.error('Erro:', error);
-            });
+if (!brandId) {
+    const div = document.getElementById('info');
+    if (div) {
+        div.innerHTML = "<p>Marca não especificada na URL.</p>";
     }
+} else {
+    fetch('/pages/brands/brand.json') 
+        .then(response => response.json())
+        .then(brands => {
+            const brand = brands.find(m => m.id.toLowerCase() === brandId.toLowerCase());
+
+            const main = document.getElementById('info');
+
+            if (main) {
+                if (brand) {
+                    main.innerHTML = `
+                        <div class="card">
+                            <img src="${brand.logo}" alt="${brand.name}" class="brand-logo">
+                            <carrossel-component></carrossel-component>
+                            <cards-container></cards-container>
+                        </div>
+                    `;
+                } else {
+                    div.innerHTML = `<p>Marca não encontrada</p>`; 
+                }
+            }
+        })
+        .catch(error => {
+            const div = document.getElementById('info');
+            if (div) {
+                div.innerText = "Erro ao carregar os dados.";
+            }
+            console.error('Erro:', error);
+        });
+}
