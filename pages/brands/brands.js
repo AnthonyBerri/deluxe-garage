d01c626f2ccf3fa3cc722aa955672c292989ee2a
Ana Beatriz Adriano da Silva <ana.beatriz.adriano.silva.1929@gmail.com>
2025-06-10 20:25:11 -0300

    cardBrand.js criado, ele vai mostrar os carros de acordo com o id passado na URL

--- pages/brands/brands.js
+++ pages/brands/brands.js
@@ -1,38 +1,43 @@
-const params = new URLSearchParams(window.location.search);
-const brandId = params.get('id');
+    const params = new URLSearchParams(window.location.search);
+    const brandId = params.get('id');
 
-if (!brandId) {
-    const div = document.getElementById('info');
-    if (div) {
-        div.innerHTML = "<p>Marca não especificada na URL.</p>";
-    }
-} else {
-    fetch('/pages/brands/brand.json') 
-        .then(response => response.json())
-        .then(brands => {
-            const brand = brands.find(m => m.id.toLowerCase() === brandId.toLowerCase());
+    if (!brandId) {
+        const div = document.getElementById('info');
+        if (div) {
+            div.innerHTML = "<p>Marca não especificada na URL.</p>";
+        }
+    } else {
+        fetch('/pages/brands/brand.json') 
+            .then(response => response.json())
+            .then(brands => {
+                const brand = brands.find(m => m.id === brandId);
 
-            const main = document.getElementById('info');
+                const main = document.getElementById('info');
 
-            if (main) {
-                if (brand) {
-                    main.innerHTML = `
-                        <div class="card">
-                            <img src="${brand.logo}" alt="${brand.name}" class="brand-logo">
-                            <carrossel-component></carrossel-component>
-                            <cards-container></cards-container>
-                        </div>
-                    `;
-                } else {
-                    div.innerHTML = `<p>Marca não encontrada</p>`; 
+                if (main) {
+                    if (brand) {
+                        main.innerHTML = `
+                            <div class="card">
+                                <img src="${brand.logo}" alt="${brand.name}" class="brand-logo">
+                                <carrossel-component></carrossel-component>
+                                <cards-brands></cards-brands>  
+                            </div>
+                        `;
+
+                        const cardsContainer = document.querySelector('cards-brands');
+                        if (cardsContainer && brand.cars) {
+                            cardsContainer.setAttribute('data-cars', JSON.stringify(brand.cars));
+                        }
+                    } else {
+                        div.innerHTML = `<p>Marca não encontrada</p>`; 
+                    }
+                }   
+            })
+            .catch(error => {
+                const div = document.getElementById('info');
+                if (div) {
+                    div.innerText = "Erro ao carregar os dados.";
                 }
-            }
-        })
-        .catch(error => {
-            const div = document.getElementById('info');
-            if (div) {
-                div.innerText = "Erro ao carregar os dados.";
-            }
-            console.error('Erro:', error);
-        });
-}
+                console.error('Erro:', error);
+            });
+    }
